a3ee09b4d765f08ed4e2298961b1f62371f47770
Chitra35006 <ch18.cse@gmail.com>
2024-09-20 16:08:41 +0600

    adding carousel+cart section

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,10 @@
 const menuBtn = document.getElementById('menu-btn');
 const mobileMenuEL = document.getElementById("mobileMenu");
-
+console.log(menuBtn.children[0]);
+    console.log(menuBtn.children);
 menuBtn.addEventListener('click',function(){
     menuBtn.children[0].classList.toggle('hidden');
+    
     const menuCloseBtn = document.getElementById('close-icon');
     menuCloseBtn.classList.toggle('hidden');
     mobileMenuEL.classList.toggle('hidden');
